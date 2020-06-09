07f61d3c5936c47a31532fd946c91e0f29e2d425
kjkandrea <32591477+kjkandrea@users.noreply.github.com>
2020-06-09 13:35:10 +0900

    불필요 소스 삭제

--- layouts/default.vue
+++ layouts/default.vue
@@ -47,20 +47,7 @@
           align="center"
           justify="center"
         >
-          <v-tooltip right>
-            <template v-slot:activator="{ on }">
-              <v-btn
-                :href="source"
-                icon
-                large
-                target="_blank"
-                v-on="on"
-              >
-                <v-icon large>mdi-code-tags</v-icon>
-              </v-btn>
-            </template>
-            <span>Source</span>
-          </v-tooltip>
+         <nuxt />
         </v-row>
       </v-container>
     </v-content>
